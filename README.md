92c559f31e7bf4d57a985f7e0bbaaf0d2aa6fa7a
= <hornet.dev.0802@gmail.com>
2023-06-01 01:42:34 -0400

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Deployment files for xypnox.com
+This repo contains the deployment files for xypnox.com.
+
+
+
+The code for xypnox.com has been rewritten in react and is not being made available for everyone to stop rampant duplication. To quote [Josh W Comeau](https://twitter.com/):
+
+> Here's the thing: this effect is effective *because* it's rare. I'm not interested in commoditizing it, because it would lose its charm!
+>
+> Instead, I'd much rather teach folks how to create effects like this, and let  them run with it. This code will live in your Git repo, not buried in a `node_modules` folder. Tinker with it, and see what else it can do!
+
+
+
+This means that the source code shall not be, for the foreseeable future, made available to the public.
+
+But this doesn't mean it is entirely proprietary either. You can always reach out to me (Twitter: [@xypnox](https://twitter.com/), and Telegram: [@xypnox](https://t.me/)) (Find my mail in resume!). And I assure you, if you do, I will help you implement any part of the website you fancy. Just don't straight up ask for the code and use it as a template. Be an artist, take inspiration and create your own masterpiece.
---
+++ precache-manifest.bf9c0e600b058580320d9915ae0976d7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8253263cfdfe58953ccb1fd8d69bf846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb6179bbba5465589b5",
+    "url": "/static/css/main.8d80cd1f.chunk.css"
+  },
+  {
+    "revision": "b6d3acecea1210426e25",
+    "url": "/static/js/2.54523976.chunk.js"
+  },
+  {
+    "revision": "d6c216e02fc1ef7a85d28c1c3d094415",
+    "url": "/static/js/2.54523976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb6179bbba5465589b5",
+    "url": "/static/js/main.10b8bfac.chunk.js"
+  },
+  {
+    "revision": "13c597a67074611b70b8",
+    "url": "/static/js/runtime-main.0fdeae1d.js"
+  },
+  {
+    "revision": "a7f4d19938f951fb0f4f3cfac97133e4",
+    "url": "/static/media/backend.a7f4d199.svg"
+  },
+  {
+    "revision": "bdd2de48c48e4338e5f8b8da28977a06",
+    "url": "/static/media/blag.bdd2de48.png"
+  },
+  {
+    "revision": "46723c75258dcf9773e6fbc7039ffa07",
+    "url": "/static/media/code.46723c75.svg"
+  },
+  {
+    "revision": "f69cdc07607fe047e002afdf57df31fa",
+    "url": "/static/media/design.f69cdc07.svg"
+  },
+  {
+    "revision": "b5178dfcabb8e99716700eb82a998d04",
+    "url": "/static/media/dotfiles.b5178dfc.png"
+  },
+  {
+    "revision": "00f3a2a1af11dd754ccc22f7bbace4ed",
+    "url": "/static/media/fifthtry.00f3a2a1.svg"
+  },
+  {
+    "revision": "d5fcf31563ac8feadb711368425f086f",
+    "url": "/static/media/finbox-logo.d5fcf315.svg"
+  },
+  {
+    "revision": "988603c08048c7dfba0c37c68dd4bd28",
+    "url": "/static/media/fontstaller.988603c0.png"
+  },
+  {
+    "revision": "ffd3f1c8d7f9e1024cae332c4da80117",
+    "url": "/static/media/frontend.ffd3f1c8.svg"
+  },
+  {
+    "revision": "d2d1a2fbcb31bf8d82db8ad1ac168efe",
+    "url": "/static/media/gyft2.d2d1a2fb.png"
+  },
+  {
+    "revision": "439aaba138382dfac71daac80da99b12",
+    "url": "/static/media/gymkhana.439aaba1.png"
+  },
+  {
+    "revision": "34e2ac54912c8d66ff2ef42bc290c6f5",
+    "url": "/static/media/kalrav.34e2ac54.png"
+  },
+  {
+    "revision": "9095a44eed5bb25c99b2b2ac78daf5a8",
+    "url": "/static/media/koss.9095a44e.png"
+  },
+  {
+    "revision": "37e278f4c31f75f4a37a358b693d01e3",
+    "url": "/static/media/kwoc.37e278f4.png"
+  },
+  {
+    "revision": "c239e495af6c18abfbdc7c1a6dd30819",
+    "url": "/static/media/logo.c239e495.svg"
+  },
+  {
+    "revision": "d1b1f35ec66b6ed18637e041955feee4",
+    "url": "/static/media/lttkgp.d1b1f35e.svg"
+  },
+  {
+    "revision": "c2ade9691aec07ca5da08a83ae9f2f0b",
+    "url": "/static/media/marg.c2ade969.png"
+  },
+  {
+    "revision": "9e75ec79a6444b0092aea10e1a67b86a",
+    "url": "/static/media/poems.9e75ec79.png"
+  },
+  {
+    "revision": "1357e3ef0592ca816ad4a3424bfc0c26",
+    "url": "/static/media/posters.1357e3ef.png"
+  },
+  {
+    "revision": "f5cc82c71c7c59bc3e7f547ba91eeb28",
+    "url": "/static/media/superpowers.f5cc82c7.svg"
+  },
+  {
+    "revision": "9a41436346276b1555cb23049bfcd341",
+    "url": "/static/media/todxpy.9a414363.png"
+  },
+  {
+    "revision": "eb223338f0ebc5011e8655c0e03c1a6b",
+    "url": "/static/media/ui-ux.eb223338.svg"
+  },
+  {
+    "revision": "8be412c4c4f9876556665907b40e3b5b",
+    "url": "/static/media/work-art.8be412c4.svg"
+  },
+  {
+    "revision": "54532b7a950770d93f8b3fda8c670b0f",
+    "url": "/static/media/xydark.54532b7a.png"
+  }
+]);
